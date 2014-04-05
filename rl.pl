593528c0eda14cf22f042e939cb09e56a7b239cb
init <initd5@gmail.com>
2014-04-05 08:38:07 -0700

    Update rl.pl

--- rl.pl
+++ rl.pl
@@ -15,5 +15,5 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Folowing code is a complete and working roguelike game, in 425 characters:
-use Curses;sub a{@l=0*initscr;map{($_%80-$n%80)**2+abs($_-$n)/80<6?$l[$_]=rand 99<2?4%~($p=$_):1:0for@w,$n=$p+$n%4}0..99;1until$l[$v=rand@w]&$l[$p=rand@w]}for(@w=0..1920;@m=(-1,0,80,-80,1);$l[$v]=2){addch$_/80,$_%80,$_^$p?("#",".",">",0,B)[$l[$_]]:"@"for@w;$p=$l[$z=$p+$m[ord(getch)-104]]&3?$z:$p;$l[$z]%=3;a if$l[$p]&2;$l[$_]&4?$l[$l[$n]&1&&$n^$p&&$l[$n]<3?$n:$_]=9|($n=$_+$m[rand 4+($l[$_]=1)]):0 for@w;$_=$_>3?4:$_ for@l}
+# Folowing code is a complete and working roguelike game, in 400 characters:
+use Curses;for(@w=0..1920;@m=(-1,clear,80,-80,1);$l[$v]=2){addch$_^$p?("#",".",">",0,B)[$l[$_]]:"@"for@w;$p=$z if$l[$z=$p+$m[ord(getch)-104]]&3;$l[$z]%=3;if($l[$p]&2){@l=0*initscr;map{($_%80-$n%80)**2+abs($_-$n)/80<6?$l[$_]=rand 99<2?4%~($p=$_):1:0for@w,$n=$p+$n%4}0..99;1until$l[$v=rand@w]&$l[$p=rand@w]}$l[$_]&4?$l[$l[$n]==1&&$n^$p?$n:$_]=9|($n=$_+$m[rand 4+($l[$_]=1)]):0for@w;map$_=$_>3?4:$_,@l}
